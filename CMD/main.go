a21aa48d94e297820841e29f66a2fe70e76d740c
Muralidhar <66506079+murali1999-tech@users.noreply.github.com>
2023-05-05 14:16:48 +0530

    Update main.go

--- CMD/main.go
+++ CMD/main.go
@@ -6,45 +6,41 @@ import (
 	"os"
 	"strings"
 
-	"github.com/poker-game/internal/game"
-	"github.com/poker-game/internal/hand"
-	"github.com/poker-game/internal/player"
-	"github.com/poker-game/pkg/utils"
+	"github.com/murali1999-tech/poker-game/internal/game"
+	"github.com/murali1999-tech/poker-game/internal/hand"
+	"github.com/murali1999-tech/poker-game/internal/player"
 )
 
 func main() {
-	// Create two players
-	player1 := player.New("Player 1")
-	player2 := player.New("Player 2")
+	reader := bufio.NewReader(os.Stdin)
+	p1Wins, p2Wins := 0, 0
 
-	// Create a new game
-	pokerGame := game.New(player1, player2)
-
-	// Read the input from STDIN
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for scanner.Scan() {
-		// Parse the input into a Hand
-		input := strings.TrimSpace(scanner.Text())
-		h, err := hand.Parse(input)
+	for {
+		input, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error parsing hand: %v\n", err)
-			continue
+			break
 		}
 
-		// Add the Hand to the game
-		pokerGame.AddHand(h)
+		cards := strings.Fields(input)
+		if len(cards) != 10 {
+			continue // skip invalid input
+		}
+
+		p1Cards, p2Cards := cards[:5], cards[5:]
+		p1Hand := hand.New(p1Cards)
+		p2Hand := hand.New(p2Cards)
+
+		p1 := player.New(1, p1Hand)
+		p2 := player.New(2, p2Hand)
+
+		win, _ := game.Play(p1, p2)
+		if win == 1 {
+			p1Wins++
+		} else {
+			p2Wins++
+		}
 	}
 
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error reading input: %v\n", err)
-		os.Exit(1)
-	}
-
-	// Play the game
-	pokerGame.Play()
-
-	// Print the results
-	utils.PrintResults(pokerGame.Results())
+	fmt.Printf("Player 1: %d hands\n", p1Wins)
+	fmt.Printf("Player 2: %d hands\n", p2Wins)
 }
-
